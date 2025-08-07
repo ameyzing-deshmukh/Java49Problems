213ba55f661f598c0f8c9bb67c3372cee04e500c
amey_deshmukh <ameydeshmukh1990@gmail.com>
2025-08-07 08:44:38 +0530

    Basic GraphQL get books and save book operations

--- src/main/java/org/problems/graphql/simpledemo/resolver/BookMutationResolver.java
+++ src/main/java/org/problems/graphql/simpledemo/resolver/BookMutationResolver.java
@@ -1,7 +1,22 @@
 package org.problems.graphql.simpledemo.resolver;
 
+import org.problems.graphql.simpledemo.entity.BookEntity;
+import org.problems.graphql.simpledemo.service.BookService;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class BookMutationResolver {
+
+    private final BookService bookService;
+
+    public BookMutationResolver(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @MutationMapping
+    public BookEntity saveBook(@Argument String name, @Argument Integer cost, @Argument String author){
+        return bookService.saveBook(name, cost, author);
+    }
 }
--- src/main/java/org/problems/graphql/simpledemo/resolver/BookQueryResolver.java
+++ src/main/java/org/problems/graphql/simpledemo/resolver/BookQueryResolver.java
@@ -1,7 +1,22 @@
 package org.problems.graphql.simpledemo.resolver;
 
+import org.problems.graphql.simpledemo.entity.BookEntity;
+import org.problems.graphql.simpledemo.service.BookService;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class BookQueryResolver {
+
+    private final BookService bookService;
+
+    public BookQueryResolver(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @QueryMapping
+    public List<BookEntity> getAllBooks(){
+        return bookService.getAllBooks();
+    }
 }
